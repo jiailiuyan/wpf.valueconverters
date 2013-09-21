7663821e4b16f0b3c6112dad20bdbe9cc4dbe7c1
resnyanskiy <resnyanskiy@gmail.com>
2013-09-21 10:24:27 +0700

    feature(HasPropertyConverter)
    Check if source object has property with name provided by ConverterParameter.

---
+++ ValueConverters/ConverterBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Community.Windows.ValueConverters
+{
+  public abstract class ConverterBase<T> : MarkupExtension, IValueConverter where T : class, new()
+  {
+    public abstract object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture);
+
+    public virtual object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+      return this;
+    }
+  }
+}
---
+++ ValueConverters/HasPropertyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Community.Windows.ValueConverters
+{
+  [ValueConversion(typeof(object), typeof(bool), ParameterType = typeof(string))]
+  public class HasPropertyConverter : ConverterBase<HasPropertyConverter>
+  {
+    public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      var propertyName = parameter as string;
+      if(value != null && !string.IsNullOrEmpty(propertyName))
+      {
+        return value.GetType().GetProperty(propertyName) != null;
+      }
+
+      return false;
+    }
+  }
+}

5ba0aec9d5e776ec992d1a3d68310de86329b020
resnyanskiy <resnyanskiy@gmail.com>
2013-09-23 08:47:54 +0700

    feature(EqualityToVisibilityConverter)
    Check if source object ToString() result equal to ConverterParameter and retrurn Visibility.

---
+++ ValueConverters/EqualityToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Community.Windows.ValueConverters
+{
+  [ValueConversion(typeof(object), typeof(Visibility), ParameterType = typeof(string))]
+  public class EqualityToVisibilityConverter : ConverterBase<EqualityToVisibilityConverter>
+  {
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      var stringToCompare = (string)parameter;
+      if(value != null && !string.IsNullOrEmpty(stringToCompare))
+      {
+        return stringToCompare.Equals(value.ToString()) ? Visibility.Visible : Visibility.Hidden;
+      }
+
+      return false;
+    }
+  }
+}

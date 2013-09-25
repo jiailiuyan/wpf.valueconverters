4ced11018f0bfc56c0f37b00284285c649a80b83
resnyanskiy <resnyanskiy@gmail.com>
2013-09-25 15:25:25 +0700

    feature(Sandbox) WPF application for viewing value converters in action

---
+++ Sandbox/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sandbox.ViewModels
+{
+  internal class MainWindowViewModel : ViewModelBase
+  {
+    public MainWindowViewModel()
+    {
+      Items = new List<Item>
+        {
+          new Item(),
+          new ItemWithTime {Time = DateTime.Now}
+        };
+    }
+
+    public IEnumerable<Item> Items { get; private set; }
+    public Item SelectedItem { get; set; }
+  }
+
+  internal class Item
+  {
+  }
+
+  internal class ItemWithTime : Item
+  {
+    public DateTime Time { get; set; }
+  }
+}
---
+++ Sandbox/ViewModels/ViewModelBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Sandbox.Annotations;
+
+namespace Sandbox.ViewModels
+{
+  internal abstract class ViewModelBase : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      if(PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+
+  // http://msdn.microsoft.com/en-us/magazine/dd419663.aspx
+  public class RelayCommand : ICommand
+  {
+    private readonly Predicate<object> _canExecute;
+    private readonly Action<object> _execute;
+
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+      if(execute == null)
+        throw new ArgumentNullException("execute");
+
+      _execute = execute;
+      _canExecute = canExecute;
+    }
+
+    public RelayCommand(Action<object> execute) : this(execute, null)
+    {
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return _canExecute == null || _canExecute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+      add { CommandManager.RequerySuggested += value; }
+      remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public void Execute(object parameter)
+    {
+      _execute(parameter);
+    }
+  }
+}

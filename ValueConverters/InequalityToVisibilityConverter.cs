181aae13ff69e2eb4f6c88717d0f58f63f52e089
resnyanskiy <resnyanskiy@gmail.com>
2013-09-26 11:05:32 +0700

    feature(InequalityToVisibilityConverter)

---
+++ ValueConverters/InequalityToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Community.Windows.ValueConverters
+{
+  [ValueConversion(typeof(object), typeof(Visibility), ParameterType = typeof(string))]
+  public class InequalityToVisibilityConverter : ConverterBase<InequalityToVisibilityConverter>
+  {
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      var stringToCompare = (string)parameter;
+      if(value != null && !string.IsNullOrEmpty(stringToCompare))
+      {
+        return stringToCompare.Equals(value.ToString()) ? Visibility.Hidden : Visibility.Visible;
+      }
+
+      return false;
+    }
+  }
+}

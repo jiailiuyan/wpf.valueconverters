f1c5745af05bc1f2b0db39fba19abf790feaeeb4
resnyanskiy <resnyanskiy@gmail.com>
2013-10-02 09:43:10 +0700

    refactor(Invert) Added 'Invert' parameter

--- ValueConverters/BooleanToVisibilityConverter.cs
+++ ValueConverters/BooleanToVisibilityConverter.cs
@@ -8,14 +8,18 @@ namespace Community.Windows.ValueConverters
   [ValueConversion(typeof(bool), typeof(Visibility))]
   public class BooleanToVisibilityConverter : ConverterBase
   {
+    public bool Invert { get; set; }
+
     public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-      return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+      var flag = (value is bool && (bool)value);
+      return (flag ^ Invert) ? Visibility.Visible : Visibility.Collapsed;
     }
 
     public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-      return value is Visibility && (Visibility)value == Visibility.Visible;
+      var result = value is Visibility && (Visibility)value == Visibility.Visible;
+      return result ^ Invert;
     }
   }
 }
--- ValueConverters/EqualityToVisibilityConverter.cs
+++ ValueConverters/EqualityToVisibilityConverter.cs
@@ -8,12 +8,15 @@ namespace Community.Windows.ValueConverters
   [ValueConversion(typeof(object), typeof(Visibility), ParameterType = typeof(string))]
   public class EqualityToVisibilityConverter : ConverterBase
   {
+    public bool Invert { get; set; }
+
     public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
       var stringToCompare = (string)parameter;
       if(value != null && !string.IsNullOrEmpty(stringToCompare))
       {
-        return stringToCompare.Equals(value.ToString()) ? Visibility.Visible : Visibility.Hidden;
+        var flag = stringToCompare.Equals(value.ToString());
+        return flag ^ Invert ? Visibility.Visible : Visibility.Hidden;
       }
 
       return false;
